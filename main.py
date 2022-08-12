7033e13438575e03d4fdcb7787f4728a58272677
Simon <simonnchong@gmail.com>
2022-08-12 15:40:59 +0800

    added features
    user control to flip the card, display completed learning message

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-
-
 #* ----------------------------------------------------- READ CSV FILE -----------------------------------------
 
 try:
-    df = pd.read_csv("./data/words_to_learn.csv")
+    df = pd.read_csv("./data/words_to_learn.csv") # read the Chinese and English words that has been learned by the user from the csv
 except FileNotFoundError:
     df = pd.read_csv("./data/english-chinese.csv") # read the csv file
 finally: # if the code in "try" block is failed or succeed, get the data from `english-chinese.csv`
     to_learn = df.to_dict(orient="records") # convert the dataframe into a well formatted dictionary in a list, orient = 'dict', list, 'series', 'split', 'records', 'index'
     
-
 # print(data_dict)
 
 #* ------------------------------------------------ UPDATE GUI WHEN BUTTON IS CLICKED ----------------------------------
@@ -23,13 +20,21 @@ random_word_dict = {}
 
 
 def next_card():
-    global random_word_dict, flip_card_timer
-    window.after_cancel(flip_card_timer) # cancel the timer loop when user press the button, means start over the timer countdown
-    random_word_dict = choice(to_learn) # random pick a pair of word from the data dictionary
-    canvas.itemconfig(card_color, image=front_image) # change to front card
-    canvas.itemconfig(canvas_title, text="English", fill="black") # change the title to "English" and text color to black
-    canvas.itemconfig(canvas_word, text=random_word_dict["English"], fill="black") # change to next English word from the random picked dictionary
-    flip_card_timer = window.after(3000, func=flip_card) # start the timer countdown to flip the card
+    global random_word_dict
+    # window.after_cancel(flip_card_timer) # cancel the timer loop when user press the button, means start over the timer countdown
+    if len(to_learn) != 0: # check if there is still some more words need to learn
+        random_word_dict = choice(to_learn) # random pick a pair of word from the data dictionary
+        canvas.itemconfig(card_color, image=front_image) # change to front card
+        canvas.itemconfig(canvas_title, text="English", fill="black") # change the title to "English" and text color to black
+        canvas.itemconfig(canvas_word, text=random_word_dict["English"], fill="black") # change to next English word from the random picked dictionary
+        # flip_card_timer = window.after(3000, func=flip_card) # start the timer countdown to flip the card
+    else: # if no more word need to learn
+        canvas.itemconfig(canvas_title, text="", fill="black") # change the title to "English" and text color to black
+        canvas.itemconfig(canvas_word, text="You've learned all the words! Congratulation!", fill="black", width=770) # change to next English word from the random picked dictionary
+        wrong_button.config(state=DISABLED)
+        flip_card_button.config(state=DISABLED)
+        right_button.config(state=DISABLED)
+        
 
     
 def flip_card():
@@ -58,7 +63,7 @@ def right_button():
 window = Tk()
 window.title("English to Chinese Flash Card Application")
 window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
-flip_card_timer = window.after(3000, func=flip_card) # start the timer when user first run the program to flip the card
+# flip_card_timer = window.after(3000, func=flip_card) # start the timer when user first run the program to flip the card
 
 front_image = PhotoImage(file="./images/card_front.png")
 back_image = PhotoImage(file="./images/card_back.png")
@@ -67,15 +72,19 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 card_color = canvas.create_image(400, 263, image=front_image)
 canvas_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
 canvas_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
-canvas.grid(row=0, column=0, columnspan=2)
+canvas.grid(row=0, column=0, columnspan=3)
 
 wrong_button_img = PhotoImage(file="./images/wrong.png",)
 wrong_button = Button(image=wrong_button_img, command=next_card)
 wrong_button.grid(row=1, column=0)
 
+flip_card_image = PhotoImage(file="./images/see_translation.png")
+flip_card_button = Button(image=flip_card_image, command=flip_card, bg=BACKGROUND_COLOR, highlightthickness=0)
+flip_card_button.grid(row=1, column=1)
+
 right_button_img = PhotoImage(file="./images/right.png")
 right_button = Button(image=right_button_img, command=right_button)
-right_button.grid(row=1, column=1)
+right_button.grid(row=1, column=2)
 
 next_card() # generate a random word when run the program
 
